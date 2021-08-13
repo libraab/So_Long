098519840bb76ec80511b71ee584ba2929dca459
Asma Bouhlel boughdira <abouhlel@c2r5p1.42nice.fr>
2021-08-13 22:26:00 +0200

    nocturne

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abouhlel <abouhlel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 17:25:00 by abouhlel          #+#    #+#             */
-/*   Updated: 2021/08/13 01:08:48 by abouhlel         ###   ########.fr       */
+/*   Updated: 2021/08/13 22:24:59 by abouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ void	ft_print_map(t_main *win)
 	}
 }
 
-int	get_width(char *file)
+int	get_height(char *file)
 {
 	int fd;
 	int count;
@@ -69,7 +69,7 @@ int	get_width(char *file)
 	return (count);
 }
 
-int	get_height(char *file)
+int	get_width(char *file)
 {
 	int fd;
 	int i;
@@ -114,7 +114,26 @@ int deal_key(int key, t_main *win)
 			win->player_pos.y += 50;
 	}
 	ft_put_image_to_image(&win->map_img, &win->player_right, win->player_pos.x, win->player_pos.y);
-	mlx_put_image_to_window(win->mlx_ptr, win->win_ptr, win->map_img.img, 0, 0);
+
+		int y_win = 0; //camera position work here 
+		int x_win = 0;
+		
+		if (win->player_pos.y > 250)
+		{
+			y_win = win->player_pos.y * -1;
+			if (win->player_pos.y > win->map_img.height - 500)
+				y_win = (win->map_img.height - 500) * -1;
+			// printf("%d %d %d\n", y_win, win->map_img.height, win->player_pos.y);
+			
+		}
+		if (win->player_pos.x > 250)
+		{
+			x_win = win->player_pos.x * -1 + 200;
+			if (win->player_pos.x > win->map_img.width - 500)
+			 	x_win = (win->map_img.width - 500) * -1;
+			// printf("%d %d %d\n", x_win, win->map_img.width, win->player_pos.x);
+		}
+	mlx_put_image_to_window(win->mlx_ptr, win->win_ptr, win->map_img.img, x_win, y_win);
     return (0);
 }
 
@@ -129,33 +148,38 @@ int	main(int ac, char **av)
 	if (ft_parsing(&win, av[1]))
 	{
 		printf ("%d %d\n", get_width(av[1]), get_height(av[1]));
-		win.map_img.width = get_width(av[1]) * 50;
 		win.map_img.height = get_height(av[1]) * 50;
-		if (win.map_img.width < 500 && win.map_img.height < 500)
-			win.win_ptr = mlx_new_window(win.mlx_ptr, win.map_img.height, win.map_img.width, "so_long");
-		else if (win.map_img.height < 500)
-			win.win_ptr = mlx_new_window(win.mlx_ptr, win.map_img.height, 500, "so_long");
+		win.map_img.width = get_width(av[1]) * 50;
+		if (win.map_img.height < 500 && win.map_img.width < 500)
+			win.win_ptr = mlx_new_window(win.mlx_ptr, win.map_img.width, win.map_img.height, "so_long");
 		else if (win.map_img.width < 500)
-			win.win_ptr = mlx_new_window(win.mlx_ptr, 500, win.map_img.width, "so_long");
+			win.win_ptr = mlx_new_window(win.mlx_ptr, win.map_img.width, 500, "so_long");
+		else if (win.map_img.height < 500)
+			win.win_ptr = mlx_new_window(win.mlx_ptr, 500, win.map_img.height, "so_long");
 		else
 			win.win_ptr = mlx_new_window(win.mlx_ptr, 500, 500, "so_long");
-		win.map_img.img = mlx_new_image(win.mlx_ptr, win.map_img.height, win.map_img.width);
+		win.map_img.img = mlx_new_image(win.mlx_ptr, win.map_img.width, win.map_img.height);
 		ft_print_map(&win);
 		mlx_hook(win.win_ptr, 2, 1L<<0, deal_key, &win);
 		
 		int y_win = 0; //camera position work here 
 		int x_win = 0;
-
+		
 		if (win.player_pos.y > 250)
 		{
 			y_win = win.player_pos.y * -1;
-			if (y_win > win.map_img.width * -1)
-				y_win = (win.map_img.width - 500) * -1;
-			printf("%d %d %d\n", y_win, win.map_img.width, win.player_pos.y);
-			// if (y_win > win.player_pos.y)
-			// 	y_win = win.player_pos.y;
+			if (win.player_pos.y > win.map_img.height - 500)
+				y_win = (win.map_img.height - 500) * -1;
+			printf("%d %d %d\n", y_win, win.map_img.height, win.player_pos.y);
+			
 		}
-		
+		if (win.player_pos.x > 250)
+		{
+			x_win = win.player_pos.x * -1 + 200;
+			if (win.player_pos.x > win.map_img.width - 500)
+			 	x_win = (win.map_img.width - 500) * -1;
+			printf("%d %d %d\n", x_win, win.map_img.width, win.player_pos.x);
+		}
 		mlx_put_image_to_window(win.mlx_ptr, win.win_ptr, win.map_img.img, x_win, y_win);
 		
 		mlx_loop(win.mlx_ptr);
